d8ca4c7360088545fbcfb3c2145ed71918006a1d
kirill.buga <kirill.buga@binary-studio.com>
2014-08-02 21:46:34 +0300

    Revert ""Enable SSL"
    This reverts commit 503404a34e835cf4c95d4fed4e47878ce58e0766.

--- Azimuth/App_Start/FilterConfig.cs
+++ Azimuth/App_Start/FilterConfig.cs
@@ -8,7 +8,6 @@ namespace Azimuth
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-//            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
--- Azimuth/Controllers/HomeController.cs
+++ Azimuth/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using System.Web.Mvc;
 
 namespace Azimuth.Controllers
 {
-    [RequireHttps]
     public class HomeController : Controller
     {
         public ActionResult Index()
