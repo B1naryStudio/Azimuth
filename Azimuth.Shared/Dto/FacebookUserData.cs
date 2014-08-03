acb1158d64d454752a6e28486b435700671f7bb3
Beseda Dmitriy <BesedaDG@gmail.com>
2014-08-04 00:32:18 +0300

    Added FacebookUserData dto class for gettinf user data from facebook api and unparse it from JSON

---
+++ Azimuth.Shared/Dto/FacebookUserData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azimuth.Shared.Dto
+{
+    public class FacebookUserData
+    {
+        //[JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+        //[JsonProperty(PropertyName = "first_name")]
+        public string first_name { get; set; }
+        //[JsonProperty(PropertyName = "last_name")]
+        public string last_name { get; set; }
+        //[JsonProperty(PropertyName = "email")]
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }
+        //[JsonProperty(PropertyName = "birthday")]
+        public string birthday { get; set; }
+        //[JsonProperty(PropertyName = "timezone")]
+        public int timezone { get; set; }
+        public Photo Photo { get; set; }
+        public Location Location { get; set; }
+
+        public override string ToString()
+        {
+            return first_name + last_name + name + gender + email + birthday + timezone +
+                   Location.name;
+        }
+    }
+
+    public class Location
+    {
+        //[JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+        //[JsonProperty(PropertyName = "name")]
+        public string name { get; set; }
+    }
+
+    public class Photo
+    {
+        //[JsonProperty(PropertyName = "url")]
+        public string url { get; set; }
+    }
+    
+}
