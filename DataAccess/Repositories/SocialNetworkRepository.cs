35de4ed74be21dbfdd1660b874a1835b9ebbdb64
Beseda Dmitriy <BesedaDG@gmail.com>
2014-08-04 00:33:44 +0300

    Added SocialNetwork and UserSocialNetwork Repositories

---
+++ DataAccess/Repositories/SocialNetworkRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azimuth.DataAccess.Entities;
+using Azimuth.DataAccess.Infrastructure;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace Azimuth.DataAccess.Repositories
+{
+    public class SocialNetworkRepository:BaseRepository<SocialNetwork>
+    {
+        private ISession _session;
+
+        public SocialNetworkRepository(ISession session) : base(session)
+        {
+            _session = session;
+        }
+
+        public SocialNetwork GetSN(string name)
+        {
+            var sn = _session.Query<SocialNetwork>().Where(s => s.Name == name).ToList();
+            return sn[0];
+        }
+    }
+}
---
+++ DataAccess/Repositories/UserSocialNetworkRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azimuth.DataAccess.Entities;
+using Azimuth.DataAccess.Infrastructure;
+using NHibernate;
+
+namespace Azimuth.DataAccess.Repositories
+{
+    public class UserSocialNetworkRepository:BaseRepository<UserSocialNetwork>
+    {
+        public UserSocialNetworkRepository(ISession session): base(session)
+        {
+        }
+    }
+}
