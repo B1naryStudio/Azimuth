dcaf213ade798e23340d3a9ac88d18eb1d002f85
Beseda Dmitriy <BesedaDG@gmail.com>
2014-08-03 11:58:43 +0300

    Added entities for mappers

---
+++ DataAccess/Entities/UserSocialNetwork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azimuth.DataAccess.Infrastructure;
+using Castle.Components.DictionaryAdapter;
+
+namespace Azimuth.DataAccess.Entities
+{
+    public class UserSocialNetwork:BaseEntity
+    {
+        public virtual UserSNIdentifier Identifier { get; set; }
+        public virtual User User { get; set; }
+        public virtual SocialNetwork SocialNetwork { get; set; }
+        public virtual string ThirdPartId { get; set; }
+    }
+
+    public class UserSNIdentifier
+    {
+        public virtual long UserId { get; set; }
+        public virtual long SocialNetworkId { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            UserSNIdentifier id;
+            id = (UserSNIdentifier) obj;
+            if (UserId == id.UserId && SocialNetworkId == id.SocialNetworkId)
+            {
+                return true;
+            }
+            return false;;
+        }
+
+        public override int GetHashCode()
+        {
+            return (UserId + "|" + SocialNetworkId).GetHashCode();
+        }
+    }
+}
