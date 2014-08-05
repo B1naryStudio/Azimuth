b50600126c78a6f8b1357aae618b5185fe3fecdc
Beseda Dmitriy <BesedaDG@gmail.com>
2014-08-05 23:21:15 +0300

    TwitterAccountProvider cleanup

--- Azimuth/Infrastructure/TwitterAccountProvider.cs
+++ Azimuth/Infrastructure/TwitterAccountProvider.cs
@@ -14,8 +14,6 @@ namespace Azimuth.Infrastructure
         private readonly string _consumerKey;
         private readonly string _consumerSecret;
 
-        public string UserInfoUrl { get; set; }
-
         public TwitterAccountProvider(string userId, string accessToken, string accessTokenSecret, string consumerKey, string consumerSecret)
         {
             this._accessToken = accessToken;
@@ -23,26 +21,22 @@ namespace Azimuth.Infrastructure
             this._accessTokenSecret = accessTokenSecret;
             this._consumerKey = consumerKey;
             this._consumerSecret = consumerSecret;
-
-            //UserInfoUrl = String.Format(@"https://api.twitter.com/1.1/users/show.json?user_id={0}", _userId, accessToken);
-
         }
         public override async Task<User> GetUserInfoAsync(string email = "")
         {
-            //var service = new TwitterService("WUOz1dJWadM5NSUmgMrcPgiIa", "9tO77dgpGcQuve4MDf0ZTKuHY3TVw8QLpjRTCTxDXh9vJpQXyc");
             var service = new TwitterService(_consumerKey, _consumerSecret);
             service.AuthenticateWith(_accessToken, _accessTokenSecret);
             var user = service.GetUserProfile(new GetUserProfileOptions());
 
             return new User
             {
-                Name = new Name { FirstName = user.Name, LastName = String.Empty},
-                Birthday = String.Empty, //
-                Email = String.Empty, //
-                Gender = String.Empty, //
-                Location = new DataAccess.Entities.Location { City = user.Location, Country = String.Empty},
+                Name = new Name { FirstName = user.Name ?? String.Empty, LastName = String.Empty},
+                Birthday = String.Empty, 
+                Email = String.Empty, 
+                Gender = String.Empty, 
+                Location = new Location { City = user.Location ?? String.Empty, Country = String.Empty},
                 Timezone = -100,
-                ScreenName = user.ScreenName,
+                ScreenName = user.ScreenName ?? String.Empty,
                 Photo = user.ProfileImageUrl
             };
         }
