c113b2ec32283a59ff3e8312af44855f537cc0ec
IvanBakun <ivanbakun.new@gmail.com>
2014-08-05 11:27:43 +0300

    Created GoogleAccountProvider.cs

---
+++ Azimuth.Shared/Dto/GoogleUserData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Photo = Azimuth.Shared.Dto.Photo;
+namespace Azimuth.Shared.Dto
+{
+    public class GoogleUserData
+    {
+        public GoogleName name { get; set; }
+        public GoogleLocation[] placesLived { get; set; }
+        public Photo image { get; set; }
+        public string birthday { get; set; }
+        public string gender { get; set; }
+        public string displayName { get; set; }
+        public Email[] emails { get; set; }
+    }
+
+    public class GoogleLocation
+    {
+        public string value { get; set; }
+        public bool primary { get; set; }
+    }
+
+    public class Email
+    {
+        public string value { get; set; }
+        public string type { get; set; }
+    }
+
+    public class GoogleName
+    {
+        public string formatted { get; set; }
+        public string familyName { get; set; }
+        public string givenName { get; set; }
+        public string middleName { get; set; }
+        public string honorifixicPrefix { get; set; }
+        public string honorifixicSuffix { get; set; }
+    }
+}
---
+++ Azimuth/Infrastructure/GoogleAccountProvider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Azimuth.DataAccess.Entities;
+using Azimuth.Shared.Dto;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Location = Azimuth.DataAccess.Entities.Location;
+
+namespace Azimuth.Infrastructure
+{
+    public class GoogleAccountProvider : AccountProvider
+    {
+        private readonly string _userId;
+        private readonly string _accessToken;
+        public string UserInfoUrl { get; private set; }
+
+        public GoogleAccountProvider(string userId, string accessToken = "")
+        {
+            _userId = userId;
+            _accessToken = accessToken;
+            UserInfoUrl =
+                String.Format(
+                    @"https://www.googleapis.com/plus/v1/people/{0}?access_token={1}&fields=birthday%2CdisplayName%2Cemails%2Cgender%2Cimage%2Cname(familyName%2CgivenName)%2CplacesLived",
+                    userId,
+                    _accessToken);
+        }
+
+        public override async Task<User> GetUserInfoAsync(string email = "")
+        {
+            var response = await GetRequest(UserInfoUrl);
+
+            var userInfo = JObject.Parse(response);
+
+            var userData = JsonConvert.DeserializeObject<GoogleUserData>(userInfo.ToString());
+            var myPlace = (userData.placesLived ?? new GoogleLocation[]{}).First(p => p.primary) ?? new GoogleLocation{value = String.Empty};
+            var places = myPlace.value.Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var city = places.First();
+            var country = places.Last();
+
+            return new User()
+            {
+                Name = new Name() { FirstName = userData.name.givenName?? String.Empty, LastName = userData.name.familyName ?? String.Empty },
+                ScreenName = userData.displayName ?? String.Empty,
+                Gender = userData.gender,
+                Birthday = userData.birthday ?? String.Empty,
+                Email = userData.emails.First((e)=>e.type.Equals("account")).value ?? String.Empty,
+                Location =
+                    new Location()
+                    {
+                        City = city ?? String.Empty,
+                        Country = country ?? String.Empty
+                    },
+                Timezone = -100,
+                Photo = userData.image.url
+
+            };  
+        }
+    }
+}
