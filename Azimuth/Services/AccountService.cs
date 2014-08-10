195f79812c7d7d9fe0b23e35bb7b99c22789e0de
Nick <kovnick1@gmail.com>
2014-08-10 19:35:47 +0300

    refactor AccountService

--- Azimuth/Services/AccountService.cs
+++ Azimuth/Services/AccountService.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq;
 using Azimuth.DataAccess.Entities;
 using Azimuth.DataAccess.Infrastructure;
-using Azimuth.DataProviders.Interfaces;
 using Azimuth.Infrastructure;
 
 namespace Azimuth.Services
@@ -10,9 +8,9 @@ namespace Azimuth.Services
     public class AccountService : IAccountService
     {
         private readonly IUnitOfWork _unitOfWork;
-        private IRepository<User> _userRepository;
-        private IRepository<UserSocialNetwork> _userSNRepository;
-        private IRepository<SocialNetwork> _snRepository;
+        private readonly IRepository<User> _userRepository;
+        private readonly IRepository<UserSocialNetwork> _userSNRepository;
+        private readonly IRepository<SocialNetwork> _snRepository;
 
         public AccountService(IUnitOfWork unitOfWork)
         {
@@ -31,31 +29,30 @@ namespace Azimuth.Services
                 try
                 {
                     // Check wheter current user exists in database
-                    var getUserFromDB =
-                        _userSNRepository.Get(s => s.ThirdPartId == userCredential.SocialNetworkId).ToList();
-                    if (getUserFromDB.Count != 0)
+                    var getUserFromDB = _userSNRepository.GetOne(s => s.ThirdPartId == userCredential.SocialNetworkId);
+                    if (getUserFromDB != null)
                     {
                         // If user exists in database check his data fields for updating
-                        if (user.ToString() != getUserFromDB[0].Identifier.User.ToString())
+                        if (user.ToString() != getUserFromDB.Identifier.User.ToString())
                         {
-                            getUserFromDB[0].Identifier.User.Name = new Name { FirstName = user.Name.FirstName, LastName = user.Name.LastName };
-                            getUserFromDB[0].Identifier.User.ScreenName = user.ScreenName;
-                            getUserFromDB[0].Identifier.User.Gender = user.Gender;
-                            getUserFromDB[0].Identifier.User.Email = user.Email;
-                            getUserFromDB[0].Identifier.User.Birthday = user.Birthday;
-                            getUserFromDB[0].Identifier.User.Location = new Location { Country = user.Location.Country, City = user.Location.City };
-                            getUserFromDB[0].Identifier.User.Timezone = user.Timezone;
-                            getUserFromDB[0].Identifier.User.Photo = user.Photo;
+                            getUserFromDB.Identifier.User.Name = new Name { FirstName = user.Name.FirstName, LastName = user.Name.LastName };
+                            getUserFromDB.Identifier.User.ScreenName = user.ScreenName;
+                            getUserFromDB.Identifier.User.Gender = user.Gender;
+                            getUserFromDB.Identifier.User.Email = user.Email;
+                            getUserFromDB.Identifier.User.Birthday = user.Birthday;
+                            getUserFromDB.Identifier.User.Location = new Location { Country = user.Location.Country, City = user.Location.City };
+                            getUserFromDB.Identifier.User.Timezone = user.Timezone;
+                            getUserFromDB.Identifier.User.Photo = user.Photo;
                         }
                     }
                     else
                     {
-                        var currentSN = _snRepository.Get(s => s.Name == userCredential.SocialNetworkName).ToList();
+                        var currentSN = _snRepository.GetOne(s => s.Name == userCredential.SocialNetworkName);
 
                         _userRepository.AddItem(user);
                         _userSNRepository.AddItem(new UserSocialNetwork
                         {
-                            Identifier = new UserSNIdentifier {User = user, SocialNetwork = currentSN[0]},
+                            Identifier = new UserSNIdentifier {User = user, SocialNetwork = currentSN},
                             ThirdPartId = userCredential.SocialNetworkId,
                             AccessToken = userCredential.AccessToken,
                             TokenExpires = userCredential.AccessTokenExpiresIn
