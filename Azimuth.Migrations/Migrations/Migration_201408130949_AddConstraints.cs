a675def8c21380411e133256e3293e3219c0368d
definetux <goncharuk.m.n@gmail.com>
2014-08-13 10:36:39 +0300

    Add constraint on Playlists

---
+++ Azimuth.Migrations/Migrations/Migration_201408130949_AddConstraints.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace Azimuth.Migrations.Migrations
+{
+    [Migration(201408130949)]
+    public class Migration_201408130949_AddConstraints : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql("Alter Table PLaylists Add Constraint ConstraintAccess CHECK(Accessibilty IN ('public', 'private'))");
+        }
+
+        public override void Down()
+        {
+            Delete.UniqueConstraint("ConstraintAccess").FromTable("Playlists");
+        }
+    }
+}
