0e6d842f6724da6ef3713b7baa11c5b1148bda20
Beseda Dmitriy <BesedaDG@gmail.com>
2014-08-15 15:09:21 +0300

    Drop and Create works and Run Migrator creates only VersionInfo table

---
+++ Azimuth.Migrations/Runner.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using FluentMigrator.Runner.Processors;
+
+namespace Azimuth.Migrations
+{
+    public static class Runner
+    {
+        public class MigrationOptions : IMigrationProcessorOptions
+        {
+            public bool PreviewOnly { get; set; }
+            public string ProviderSwitches { get; set; }
+            public int Timeout { get; set; }
+        }
+
+        public static void MigrateToLatest(string connectionString)
+        {
+            // var announcer = new NullAnnouncer();
+            var announcer = new TextWriterAnnouncer(s => System.Diagnostics.Debug.WriteLine(s));
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var migrationContext = new RunnerContext(announcer)
+            {
+                Namespace = "Azimuth.Migrations"
+            };
+
+            var options = new MigrationOptions { PreviewOnly = false, Timeout = 60 };
+            var factory = new FluentMigrator.Runner.Processors.SqlServer.SqlServer2012ProcessorFactory();
+            var processor = factory.Create(connectionString, announcer, options);
+            var runner = new MigrationRunner(assembly, migrationContext, processor);
+            runner.MigrateUp(true);
+        }
+    }
+}
---
+++ Azimuth/Controllers/TestController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Azimuth.Migrations;
+
+namespace Azimuth.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Drop()
+        {
+            var dg = new DataGenerator.DataGenerator(MvcApplication.Container);
+            dg.ClearDatabase();
+            return null;
+        }
+
+        public ActionResult Create()
+        {
+            var dg = new DataGenerator.DataGenerator(MvcApplication.Container);
+            dg.GenerateData();
+            return null;
+        }
+
+        public ActionResult RunMigrator()
+        {
+            //string filePath = @"D:\Azimuth\Azimuth.Migrations\bin\Debug\Migrate.exe";
+            ////string arguments = "--provider sqlserver2012 --a $(TargetName)$(TargetExt)";
+            //string arguments = @"/connection Data Source=.\SQLEXPRESS;Initial Catalog=Azimuth;Integrated Security=True;Pooling=False D:\Azimuth\Azimuth.Migrations\bin\Debug\Azimuth.Migrations.dll";
+            //Process.Start(filePath, arguments);
+
+            string cnString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Azimuth;Integrated Security=True;Pooling=False";
+
+            Migrations.Runner.MigrateToLatest(cnString);
+
+            return null;
+        }
+	}
+}
