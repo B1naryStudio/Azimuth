b0b6b149ad70038b058b9451a26bc7e9b4373dbc
kirill.buga <kirill.buga@binary-studio.com>
2014-08-17 18:35:19 +0300

    Removed connectionString variable's duplicate.

--- Azimuth/Controllers/TestController.cs
+++ Azimuth/Controllers/TestController.cs
@@ -1,7 +1,4 @@
 ﻿using System.Web.Mvc;
-using Azimuth.DataAccess.Entities;
-using Azimuth.DataAccess.Infrastructure;
-using Azimuth.DataAccess.Repositories;
 using Azimuth.Migrations;
 
 
@@ -10,12 +7,10 @@ namespace Azimuth.Controllers
     public class TestController : Controller
     {
 #if DEBUG
-        //private string cnString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Azimuth;Integrated Security=True;Pooling=False";
+        private string cnString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Azimuth;Integrated Security=True;Pooling=False";
 #else
         private string cnString = @"Server=b3b51a6b-d6c5-42a4-baca-a3840159ce32.sqlserver.sequelizer.com;Database=dbb3b51a6bd6c542a4bacaa3840159ce32;User ID=cvvuqikyozurbwuh;Password=rya6vs3LLyqqoAbVFJoFZbiHjwurBQT3AxN2dquT6ojodhSh3ahANryqqzV77Ji5;";
 #endif
-
-        private string cnString = @"Server=b3b51a6b-d6c5-42a4-baca-a3840159ce32.sqlserver.sequelizer.com;Database=dbb3b51a6bd6c542a4bacaa3840159ce32;User ID=cvvuqikyozurbwuh;Password=rya6vs3LLyqqoAbVFJoFZbiHjwurBQT3AxN2dquT6ojodhSh3ahANryqqzV77Ji5;";
 
         //
         // GET: /Test/
