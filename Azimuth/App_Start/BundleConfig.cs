cecd9621321f310f0ee812652460ddfef52d2b40
Nick <kovnick1@gmail.com>
2014-08-24 20:59:51 +0300

    add volume slider

--- Azimuth/App_Start/BundleConfig.cs
+++ Azimuth/App_Start/BundleConfig.cs
@@ -21,12 +21,14 @@ namespace Azimuth
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/dragdealer.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/settings.css"));
+                      "~/Content/settings.css",
+                      "~/Content/dragdealer.css"));
         }
     }
 }
