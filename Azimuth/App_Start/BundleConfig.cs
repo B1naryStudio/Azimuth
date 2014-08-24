79ea6446686cd69ea311ccc5c991fc5aaf1a9ed0
Nick <kovnick1@gmail.com>
2014-08-24 16:56:23 +0300

    small refactor

--- Azimuth/App_Start/BundleConfig.cs
+++ Azimuth/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Azimuth
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/settings.css"));
         }
     }
 }
