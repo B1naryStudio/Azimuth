826463330deb401229a42da3dc8be80c12821d59
Ivan Bakun <ivanbakun.new@gmail.com>
2014-09-02 14:13:27 +0300

    Added progress slider and move scripts from index to other files

---
+++ Azimuth/Scripts/workWithPublicPlaylists.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    var volumeSlider = new SliderController({
+        sliderSelector: '#volumeSlider',
+        sliderBarClass: 'volumeBar',
+        sliderClass: 'volume',
+        dirrection: 'vertical'
+    });
+    var progressSlider = new SliderController({
+        sliderSelector: '#progressSlider',
+        sliderBarClass: 'progressBar',
+        sliderClass: 'progress',
+        backgroundSliderClass: 'cache',
+        dirrection: 'horizontall'
+    });
+
+    var audioManager = new AudioManager(volumeSlider, progressSlider);
+    var manager = new PublicPlaylistManager(audioManager);
+
+    manager.showPlaylists();
+    manager.bindListeners();
+    audioManager.bindListeners();
+});
