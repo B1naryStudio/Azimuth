b4242aece82a8b656ab6d82038a4f06ce9dc4766
Beseda Dmitriy <BesedaDG@gmail.com>
2014-09-08 18:47:36 +0300

    Implemented partial view for follow/unfollow user items

--- Azimuth/Controllers/UserProfileController.cs
+++ Azimuth/Controllers/UserProfileController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Azimuth.DataAccess.Entities;
 using Azimuth.Infrastructure.Concrete;
 using Azimuth.Services.Interfaces;
 
@@ -13,11 +16,28 @@ namespace Azimuth.Controllers
             _userService = userService;
         }
         //
-        // GET: /UserProfile/
+        // GET: /UserProfile/   
         public ActionResult Index(long? id)
         {
             var user = _userService.GetUserInfo(id ?? AzimuthIdentity.Current.UserCredential.Id);
             return View(user);
+        }
+
+        [HttpPost]
+        public PartialViewResult _FollowInfo(long? userId, string isFollower)
+        {
+            var isFollow = Convert.ToBoolean(isFollower);
+            User user = null;
+            if (isFollow && userId != null)
+            {
+               user = _userService.UnfollowPerson((long)userId);
+            }
+            else if (userId != null)
+            {
+                user = _userService.FollowPerson((long)userId);
+            }
+            ViewBag.isFollower = !isFollow;
+            return PartialView("_UserFollowInfo", user);
         }
     }
 }
