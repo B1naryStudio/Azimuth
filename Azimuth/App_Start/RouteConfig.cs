7ea7e0458a289c29cad069a628924630477ba084
Nick <kovnick1@gmail.com>
2014-09-08 03:06:56 +0300

    add ui for search

--- Azimuth/App_Start/RouteConfig.cs
+++ Azimuth/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Azimuth
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "SearchRoute",
+                url: "Search/{action}/{searchParam}",
+                defaults: new { action = "Index", searchParam = UrlParameter.Optional }
+            );
         }
     }
 }
