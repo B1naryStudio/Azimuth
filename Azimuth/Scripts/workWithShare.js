3161b06f1a4de8f5359d3483916aa8b02059964f
definetux <goncharuk.m.n@gmail.com>
2014-09-15 19:53:27 +0300

    Added audio manager to share page

--- Azimuth/Scripts/workWithShare.js
+++ Azimuth/Scripts/workWithShare.js
@@ -3,8 +3,8 @@
         sliderSelector: '#volumeSlider',
         sliderBarClass: 'volumeBar',
         sliderClass: 'volume',
-        dirrection: 'vertical'
-    });
+        dirrection: 'horizontall'
+       });
     var progressSlider = new SliderController({
         sliderSelector: '#progressSlider',
         sliderBarClass: 'progressBar',
