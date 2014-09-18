e4b36740807ad4ee979f9e9e11f58f087fcd846f
kirill.buga <kirill.buga@binary-studio.com>
2014-09-18 11:34:13 +0300

    Fixed the issue with circular dependency

--- Azimuth.Shared/Dto/FollowerModel.cs
+++ Azimuth.Shared/Dto/FollowerModel.cs
@@ -1,12 +1,10 @@
-﻿
-using Azimuth.DataAccess.Entities;
-
-namespace Azimuth.Shared.Dto
+﻿namespace Azimuth.Shared.Dto
 {
     public class FollowerModel
     {
         public long Id { get; set; }
-        public Name Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string ScreenName { get; set; }
         public string Photo { get; set; }
     }
