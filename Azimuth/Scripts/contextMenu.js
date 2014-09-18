7275cb747bd11676709dc0325a49c8034d0a9e47
Beseda Dmitriy <BesedaDG@gmail.com>
2014-09-18 09:38:53 +0300

    Fixed playlist contextmenu actions with updating playlist (now stay active current playlist)

--- Azimuth/Scripts/contextMenu.js
+++ Azimuth/Scripts/contextMenu.js
@@ -37,13 +37,14 @@
             data: JSON.stringify(tracksIds),
             contentType: 'application/json; charset=utf-8',
             success: function() {
-                var playlistId = $('#playlistTracks').find('.playlistId').text();
-                $('#playlistTracks').children().remove();
-                self.manager._getTracks(playlistId);
+                //var playlistId = $('#playlistTracks').find('.playlistId').text();
+                //$('#playlistTracks').children().remove();
+                //self.manager._getTracks(playlistId);
 
                 $('.tableRow.playlist').remove();
                 self.manager.playlistsGlobal.length = 0;
                 $('.playlist-divider').remove();
+                self.manager.activePlaylistId = playlistId;
                 self.manager.showPlaylists();
                 self.manager.setDefaultPlaylist();
             }
@@ -73,7 +74,7 @@
                     data: JSON.stringify(tracksIds),
                     contentType: 'application/json; charset=utf-8',
                     success: function() {
-                        $('#playlistTracks').children().remove();
+                        //$('#playlistTracks').children().remove();
                         self.manager._getTracks(oldPlaylist);
 
                         $('.tableRow.playlist').remove();
@@ -98,11 +99,11 @@
             data: JSON.stringify(tracksIds),
             contentType: 'application/json; charset=utf-8',
             success: function() {
-                if ($('#playlistTracks').children().length > 0) {
-                    var playlistId = $('#playlistTracks').find('.playlistId').text();
-                    $('#playlistTracks').children().remove();
-                    self.manager._getTracks(playlistId);
-                }
+                //if ($('#playlistTracks').children().length > 0) {
+                //    var playlistId = $('#playlistTracks').find('.playlistId').text();
+                //    $('#playlistTracks').children().remove();
+                //    self.manager._getTracks(playlistId);
+                //}
 
                 $('.tableRow.playlist').remove();
                 self.manager.playlistsGlobal.length = 0;
@@ -320,7 +321,7 @@ ContextMenu.prototype.selectAction = function ($currentItem, $musicList) {
                     $currentItem.append($('.draggable-item-selected').clone());
                     if ($currentItem.children().length > 0) {
                         self._removeSelectedTracksAction($currentItem, playlistId);
-                        self._removeSelectedTracksAction($currentItem, playlistId);
+                        //self._removeSelectedTracksAction($currentItem, playlistId);
                         self.$container.empty();
                         $currentItem = null;
                     }
--- Azimuth/Scripts/musicManager.js
+++ Azimuth/Scripts/musicManager.js
@@ -44,6 +44,7 @@ var MusicManager = function (manager) {
     this.$playlistsTitle = $('#playlist-header-title');
     this.$errorModal = $('#errorModal');
     this.$messageContainer = $('#message-container');
+    this.activePlaylistId = null;
 
     this._getTracks = function (plId) {
         var $this = $(this);
@@ -51,12 +52,14 @@ var MusicManager = function (manager) {
         $('.vkMusicList').find('.track').detach();
         var $activePlaylist = $('.playlist-active');
         var playlistId;
-        if ($activePlaylist.length > 1) {
+        if ($this.hasClass('playlist')) {
             playlistId = $this.children('.playlistId').text();
         } else {
             playlistId = $activePlaylist.find('.playlistId').text();
+            this.activePlaylistId = playlistId;
         }
-        if (playlistId.length == 0) {
+
+        if (playlistId == null) {
             playlistId = plId;
         }
         $.ajax({
@@ -100,10 +103,12 @@ var MusicManager = function (manager) {
             }
         });
         //$(this).next("#playlistTracksTable").slideToggle(100); // TODO replace with class name
-        var temp = self.playlistsGlobal.filter(function (index) {
-            return index.Id == playlistId;
-        })[0].Name;
-        self.$vkMusicTitle.text(temp);
+        //var temp = self.playlistsGlobal.filter(function (index) {
+        //    return index.Id == playlistId;
+        //})[0].Name;
+        //self.$vkMusicTitle.text(temp);
+
+
         self.audioManager.bindPlayBtnListeners();
 
         $('.playlist-active').toggleClass('playlist-active', false);
@@ -222,7 +227,10 @@ var MusicManager = function (manager) {
                 self.audioManager.bindPlayBtnListeners();
                 $('#vkMusicTable > .tableTitle').text("User Tracks");
                 self.$vkMusicLoadingSpinner.hide();
-                $('.vkMusicList').show();
+                var $activePlaylist = $('.playlist-active');
+                if ($activePlaylist.hasClass('default-playlist')) {
+                    $('.vkMusicList').show();
+                }
             },
             error: function () {
                 $('#vkMusicTable > .tableTitle').text("Error has occurred");
@@ -586,7 +594,6 @@ MusicManager.prototype.showPlaylists = function (playlists) {
                 console.log(playlistsData.Message);
                 if (typeof playlistsData.Message === 'undefined') {
                     self.$reloginForm.hide();
-                    self.$vkMusicTable.show();
                     self.playlists = playlistsData;
                     for (var i = 0; i < self.playlists.length; i++) {
                         var playlist = self.playlists[i];
@@ -604,6 +611,17 @@ MusicManager.prototype.showPlaylists = function (playlists) {
                         self.$playlistsTable.append(tmpl);
                         self._setChangingPlaylistImage(tmpl);
                     }
+                    if (self.activePlaylistId != null && self.activePlaylistId.length > 0) {
+                        var $active = $('.playlist span:contains(' + self.activePlaylistId +')').parents('.playlist');
+                        $('.playlist-active').toggleClass('playlist-active', false);
+                        $active.toggleClass('playlist-active', true);
+                        self._getTracks();
+                        $('.playlist-active').toggleClass('playlist-active', false);
+                        $active.toggleClass('playlist-active', true);
+                        $('.vkMusicList').show();
+                    } else {
+                        self.$vkMusicTable.show();
+                    }
                 } else {
                     self.$reloginForm.show();
                     self.$vkMusicTable.hide();
@@ -613,10 +631,9 @@ MusicManager.prototype.showPlaylists = function (playlists) {
                 $.ajax({
                     url: '/api/playlists/favorite/notOwned',
                     success: function (playlistsData) {
-                        console.log('Hi');
                         if (typeof playlistsData.Message === 'undefined') {
                             self.$reloginForm.hide();
-                            self.$vkMusicTable.show();
+                            //self.$vkMusicTable.show();
                             self.playlists = playlistsData;
                             for (var i = 0; i < self.playlists.length; i++) {
                                 var playlist = self.playlists[i];
